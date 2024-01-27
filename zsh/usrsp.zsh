83ade74b4a03227f3555c013e4d70cc10a4693c7
elvis.wenhao.wu@gmail.com <elvis.wenhao.wu@gmail.com>
2024-01-28 01:46:54 +0800

    Fix neovim path for remote connection

--- zsh/usrsp.zsh
+++ zsh/usrsp.zsh
@@ -3,9 +3,9 @@ DEFAULT_USER=$(whoami)
 
 # Preferred editor for local and remote sessions
 if [[ -n "$SSH_CONNECTION" ]]; then
-    export EDITOR='/usr/local/bin/nvim'
+    export EDITOR='/opt/homebrew/bin/nvim'
 else
-    export EDITOR='/usr/local/bin/nvim'
+    export EDITOR='/opt/homebrew/bin/nvim'
 fi
 
 # Help directory for alias
