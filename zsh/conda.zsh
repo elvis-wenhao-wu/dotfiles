73e2eccffcdda2a2dec6f3999efa679b0b006407
elvis.wenhao.wu@gmail.com <elvis.wenhao.wu@gmail.com>
2024-01-27 19:01:56 +0800

    Use raw sh to install miniconda and thus modify the path

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -1,14 +1,14 @@
 # Conda setup
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/local/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/wwu/.conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-        . "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    if [ -f "/Users/wwu/.conda/etc/profile.d/conda.sh" ]; then
+        . "/Users/wwu/.conda/etc/profile.d/conda.sh"
     else
-        export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
+        export PATH="/Users/wwu/.conda/bin:$PATH"
     fi
 fi
 unset __conda_setup
